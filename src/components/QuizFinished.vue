6d22c94a3be36ac53f7af751a94685535bfd186c
Robin Dickson <robindickson@gmail.com>
2022-05-15 12:39:03 +0900

    feat: Add styling and images to answer history

--- src/components/QuizFinished.vue
+++ src/components/QuizFinished.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="text-center">
     <h2>Finished!</h2>
-    <p>You scored {{ score }} out of {{ settings.questionAmount }}</p>
-    <br />
+    <h3>You scored {{ score }} out of {{ settings.questionAmount }}</h3>
     <div>
       <AppButton
         v-on:click="$emit('playAgain')"
@@ -19,22 +18,24 @@
     </div>
     <br />
     <h3>Review</h3>
-    <h4>Correct answers</h4>
+    <h4>Words you answered correctly:</h4>
     <ul>
       <li
+        class="quiz-answer-list-item"
         v-for="flashcard in answerHistory.correctAnswers"
         :key="flashcard.vocabulary"
       >
-        {{ flashcard.vocabulary }}
+        <img class='quiz-answer-item-emoji' :src="flashcard.imageUrl" />{{ flashcard.vocabulary }}
       </li>
     </ul>
-    <h4>Incorrect answers</h4>
-    <ul>
+    <h4>Words you answered incorrectly:</h4>
+    <ul class="quiz-answer-list">
       <li
+        class="quiz-answer-list-item"
         v-for="flashcard in answerHistory.incorrectAnswers"
         :key="flashcard.vocabulary"
       >
-        {{ flashcard.vocabulary }}
+        <img class='quiz-answer-item-emoji' :src="flashcard.imageUrl" /> {{ flashcard.vocabulary }}
       </li>
     </ul>
   </div>
@@ -50,4 +51,16 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.quiz-answer-list-item {
+  display: flex;
+  align-items: center;
+  max-width: 260px;
+  margin: 0 auto 6px auto;
+}
+.quiz-answer-item-emoji {
+  width: 25px;
+  height: 25px;
+  margin-right: 10px;
+}
+</style>
